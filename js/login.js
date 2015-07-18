b09f8b9a1a1252fd7f9ea08a52cd968dc0968a44
Aakash Adesara <aakash.adesara@gmail.com>
2015-07-18 16:14:26 -0700

    Friends done

--- js/login.js
+++ js/login.js
@@ -20,6 +20,30 @@ $( document ).ready(function() {
 });
 
 function login(username, password){
+
+	if(false){
+		$.ajax({
+			url       : 'https://api-us.clusterpoint.com/ACCOUNT_ID/DATABASE/_COMMAND',
+			type      : 'POST',
+			dataType  : 'json',
+			data      : '{"query": "<name>Test</name>"}',
+			beforeSend: function (xhr) {
+				xhr.setRequestHeader('Authorization', 'Basic ' + btoa('USERNAME:PASSWORD'));
+			},
+			success   : function (data) {
+				if (typeof success != 'undefined') {
+					success(data);
+				}
+			},
+			fail      : function (data) {
+				alert(data.error);
+				if (typeof fail != 'undefined') {
+					fail(data);
+				}
+			}
+		});
+	}
+
 	Parse.User.logIn(username, password, {
 	  success: function(user) {
 	    // Do stuff after successful login.
@@ -42,6 +66,28 @@ function signup(fName, lName, email, pLink, username, password){
 	user.set("profileLink", pLink);
 	user.set("username", username);
 	user.set("password", password);
+	user.set("friendList", []);
+	user.set("soungArray", []);
+
+	var UserObj = Parse.Object.extend("UserObj");
+	var userObj = new UserObj();
+
+	userObj.set("firstName", fName);
+	userObj.set("lastname", lName);
+	userObj.set("username", username);
+	userObj.set("profileLink", pLink);
+
+	userObj.save(null, {
+	  success: function(userObj) {
+	    // Execute any logic that should take place after the object is saved.
+	    alert('New object created with objectId: ' + gameScore.id);
+	  },
+	  error: function(userObj, error) {
+	    // Execute any logic that should take place if the save fails.
+	    // error is a Parse.Error with an error code and message.
+	    alert('Failed to create new object, with error code: ' + error.message);
+	  }
+	});
 
 	user.signUp(null, {
 	  success: function(user) {
@@ -50,6 +96,46 @@ function signup(fName, lName, email, pLink, username, password){
 	  },
 	  error: function(user, error) {
 	    // Show the error message somewhere and let the user try again.
+	    alert("Error: " + error.code + " " + error.message);
+	  }
+	});
+}
+
+function findFriend(){
+	var UserObj = Parse.Object.extend("UserObj");
+	var query = new Parse.Query(UserObj);
+	query.equalTo("username", $("#friendFinder").val());
+	query.find({
+	  success: function(results) {
+	    alert("Successfully retrieved " + results.length + " scores.");
+	    // Do something with the returned Parse.Object values
+	   
+	    var userObj = Parse.User.current();
+
+	    var newFriendList = userObj.get("friendList");
+	    newFriendList.push($("#friendFinder").val());
+
+	    alert(newFriendList);
+
+	    userObj.set("friendList", newFriendList);
+	    userObj.save(null, {
+	    	success: function(userObj) {
+			    // Execute any logic that should take place after the object is saved.
+			    alert('New object created with objectId: ' + gameScore.id);
+			 },
+			 error: function(userObj, error) {
+			    // Execute any logic that should take place if the save fails.
+			    // error is a Parse.Error with an error code and message.
+			    alert('Failed to create new object, with error code: ' + error.message);
+			 }
+	    });
+
+	    /*for (var i = 0; i < results.length; i++) {
+	      var object = results[i];
+	      alert(object.id + ' - ' + object.get('playerName'));
+	    }*/
+	  },
+	  error: function(error) {
 	    alert("Error: " + error.code + " " + error.message);
 	  }
 	});
--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
 function dashboard(user){
 	$("#login-wrapper").hide();
-	$("#topRightNavBar").html("<li><a onClick=\"location.reload();\">Signout</a></li>");
+	$("#topRightNavBar").html("<li><a onClick=\"dashboard();\">Dashboard</a></li><li><a onClick=\"chromatic();\">Chromatic</a></li><li><a onClick=\"location.reload();\">Signout</a></li>");
+	$("#content-wrapper").html("<div style=\" text-align: center;\" class=\"jumbotron col-md-4\">  <img src=\"" + user.get("profileLink") + "\" style=\"width: 200px; height: 200px;\"class=\"img-circle\"> <br>  <h1>" + user.get("firstname") + " " + user.get("lastname") + "</h1><input type=\"text\" style=\"text-align: center;\"class=\"form-control\" id=\"friendFinder\" placeholder=\"Find a friend by username \"><p><br><a class=\"btn btn-primary btn-lg\" onClick=\"findFriend();\">Add Friend</a></p></div>");
+	$("#content-wrapper").html($("#content-wrapper").html() + "<div class=\"col-md-8 jumbotron\" id=\"friendsDiv\"></div>");
+
+	for(var i = 0; i < user.get("friendList").length; i++){
+		getFriendInfo(user.get("friendList")[i]);
+	}
+}
+
+function getFriendInfo(username){
+	var UserObj = Parse.Object.extend("UserObj");
+	var userObj = new Parse.Query(UserObj);
+	userObj.equalTo("username", username);
+	userObj.find({
+	  success: function(results) {
+	    alert("Successfully retrieved " + results.length + " scores.");
+	    // Do something with the returned Parse.Object values
+	    /*for (var i = 0; i < results.length; i++) {
+	      var object = results[i];
+	      alert(object.id + ' - ' + object.get('playerName'));
+	    }*/
+	    var obj = results[0];
+	    $("#friendsDiv").html($("#friendsDiv").html() + "<div style=\"text-align: center;\"class=\"panel panel-primary col-md-4\">  <div class=\"panel-heading\">    <h3 class=\"panel-title\"><img src=\"" + obj.get("profileLink") + "\" style=\"width: 50px; height: 50px;\"class=\"img-circle\"><br>"  + obj.get("username")+ "</h3>  </div>  <div class=\"panel-body\">" + obj.get("firstName") + " " + obj.get("lastname") + "</div><a onClick=\"collab(" + obj.get("username") + ");\" class=\"btn btn-primary\">Collaborate</a><a onClick=\"contact(" + obj.get("username") + ");\" class=\"btn btn-success\">Contact</a></div>");
+	  },
+	  error: function(error) {
+	    alert("Error: " + error.code + " " + error.message);
+	  }
+	});
 }
